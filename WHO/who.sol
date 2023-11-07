fb6e2258d20238cf62efe21e6acc558f43abefeb
agustinlopez1999 <lopezagustin1999@hotmail.com>
2023-11-07 18:31:46 -0300

    viewResults

--- WHO/who.sol
+++ WHO/who.sol
@@ -86,12 +86,27 @@ contract ContractHealthCenter{
         require(_address == HealthCenter_contract,"You don't have permissions");
         _;
     }
-
+    //Example IPFS: QmUhMZ7e6WHqUVhQ2vDEY32vDTXRVQvMEgzaFA8MN7Sf2X
     //function to emit result of covid test
     function CovidTestResults(string memory _idPerson, bool _COVIDresult, string memory _IPFScode) public onlyHealthCenter(msg.sender){
         bytes32 hash_idPerson = keccak256(abi.encodePacked(_idPerson));
         COVIDresults[hash_idPerson] = results(_COVIDresult,_IPFScode);
         emit newResult(_COVIDresult,_IPFScode);
     }
+
+    //function that allows visualization of the results
+    function viewResults(string memory _idPerson) public view returns (string memory, string memory){
+        bytes32 hash_idPerson = keccak256(abi.encodePacked(_idPerson));
+        string memory testResult;
+
+        if(COVIDresults[hash_idPerson].diagnosis == true)
+            testResult = "Positive";
+        else
+            testResult = "Negative";
+        
+        return(testResult,COVIDresults[hash_idPerson].IPFScode);
+    }
+
+
 
 }
