88266234cdf37b195f3f843359a3cb7a911c88b1
agustinlopez1999 <lopezagustin1999@hotmail.com>
2023-11-07 17:04:12 -0300

    health center

---
+++ WHO/who.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+contract WHO_COVID{
+    //World Health Organization address
+    address public WHO;
+
+    //constructor
+    constructor(){
+        WHO = msg.sender;
+    }
+
+    //mapping to relate valid health centers
+    mapping(address => bool) validity_HealthCenters;
+
+    //array with addresses with valid health centers
+    address[] public health_centers_addresses;
+
+    //events
+    event NewValidHealthCenter(address);
+    event NewContract(address, address);
+
+    //modifiers that allows only WHO
+    modifier onlyWHO(address _address){
+        require(_address == WHO);
+        _;
+    }
+
+    //function that validates new health centers
+    function HealthCenter(address _healthCenter) public onlyWHO(msg.sender){
+        validity_HealthCenters[_healthCenter] = true;
+        emit NewValidHealthCenter(_healthCenter);
+    }
+
+    //function that creates a new smart contract of a health center
+    function FactoryHealthCenter() public{
+        require(validity_HealthCenters[msg.sender] == true,"You don't have permissions");
+        address HealthCenter_contract = address(new ContractHealthCenter(msg.sender));
+        health_centers_addresses.push(HealthCenter_contract);
+        emit NewContract(HealthCenter_contract,msg.sender);
+    }
+}
+
+contract ContractHealthCenter{
+    address public contractAddress;
+    constructor(address _address){
+        contractAddress = _address;
+    }
+
+}
